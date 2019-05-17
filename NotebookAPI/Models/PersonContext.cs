f7046dc64a4d956f488fd0d8107959b1fb2fe2b0
VlasovSS <VlasovSS@VLASOVSS>
2019-05-17 16:58:50 +0500

    Добавьте файлы проекта.

---
+++ NotebookAPI/Models/PersonContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NotebookAPI.Models
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactType> ContactTypes { get; set; }
+    }
+}
